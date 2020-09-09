344072ba44537d096bb5887f6e279a8cac97ed87
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-09-08 21:12:16 -0400

    Update SelectItem

--- js/src/admin/settings/items/SelectItem.js
+++ js/src/admin/settings/items/SelectItem.js
@@ -4,9 +4,9 @@ import SettingItem from './SettingItem';
 export default class SelectItem extends SettingItem {
     view() {
         return Select.component({
-            options: this.props.options,
-            value: this.getValue() || this.props.default,
-            onchange: this.setting(),
+            options: this.attrs.options,
+            value: this.getValue() || this.attrs.default,
+            bidi: this.setting(),
         });
     }
 }
