91d1a803098985f896f1f376fc1961e6be146ac1
David Sevilla Martin <me@datitisev.me>
2020-10-07 18:28:57 -0400

    Remove util/createSettingsModal
    Not used by anyone, anywhere

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,13 +1,7 @@
 import * as settingsItems from './settings/items';
 import SettingsModal from './settings/SettingsModal';
 
-import utilCreateSettingsModal from './util/createSettingsModal';
-
 export const settings = {
     SettingsModal,
     items: settingsItems,
-};
-
-export const util = {
-    createSettingsModal: utilCreateSettingsModal,
 };
